c662dc2bfa2965b0d44db989de992c3b41c2b3df
cuik00 <37495100+cuik00@users.noreply.github.com>
2020-09-01 21:25:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ account = {
 }
 license = "PLTM-M3G1H-7EK69-AL0E0-MACL5"
 tradeset = {
-	"BaseTrade":"0.005",
-	"C1":"42.12", 					#5-95
-	"C2":"45.33", 					#5-95
-	"TradeCount":"3", 				#max limit 200
+	"BaseTrade":"0.01",
+	"C1":"11.12", 					#5-95
+	"C2":"19.20", 					#5-95
+	"TradeCount":"1", 				#max limit 200
 	"MultiplyOnWin":"0", 				## 0 untuk OFF 
 	"MultiplyOnLose":"1", 			## 0 untuk OFF 
 	"MaxBaseTrade":{
@@ -22,12 +22,12 @@ tools = {
 	"AddDelayTrade":"0",				#Delay Per Trade
 	"AddDelayTradeWin":"0",				#Delay Per Trade Win
 	"AddDelayTradeLose":"0",			#Delay Per Trade Lose
-	"RecoveryMultiplier":"1.5",			# 1 untuk OFF. BaseTrade Lose Terakhir di Kali RecoveryMultiplier
+	"RecoveryMultiplier":"1.1789",			# 1 untuk OFF. BaseTrade Lose Terakhir di Kali RecoveryMultiplier
 	"RecoveryIncrease":"0",	 			# 0 untuk OFF. BaseTrade Lose Terakhir di Tambah RecoveryIncrease
-	"TargetProfit":"100",				#0 untuk OFF
-	"StopLoseBalance":"0", 			#0 untuk OFF
+	"TargetProfit":"500",				#0 untuk OFF
+	"StopLoseBalance":"500", 			#0 untuk OFF
 	"ForceTC1AfterLose":"ON",			#ON/OFF
 	"ChangeTCAfterLose":{
-		"Toogle":"ON",
+		"Toogle":"OFF",
 		"ChangeTo":"3"} 			
 }
